eefe01b2a061c1945c1e33a3a21d5db56e89d09f
Michal Svrcek <svrcekmichal@gmail.com>
2021-02-09 20:57:07 +0100

    change storyblock demos

--- stories/4-Story-Block-Context.stories.js
+++ stories/4-Story-Block-Context.stories.js
@@ -71,14 +71,14 @@ export const ArticleStoryblockV2 = () => {
       storyblockMobile={
         <div className="stretchy-wrapper d-block d-md-none">
           <div>
-            <fw-storyblock app_id="q9o_hqTL4w8E3lyFFR9UPV-21CJN1QUK" screen="mobile"></fw-storyblock>
+            <fw-storyblock channel="demoads" autoplay="off" screen="mobile"></fw-storyblock>
           </div>
         </div>
       }
       storyblockDesktop={
         <div className="stretchy-wrapper">
           <div>
-            <fw-storyblock app_id="q9o_hqTL4w8E3lyFFR9UPV-21CJN1QUK" screen="tablet,desktop"></fw-storyblock>
+            <fw-storyblock channel="demoads" screen="tablet,desktop"></fw-storyblock>
           </div>
         </div>
       }
