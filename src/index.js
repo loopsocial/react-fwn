0c0868ccf3d5dc519bc03af2ecdfbf3a4b9f91d8
Štefan Bačkor <stevo@backor.sk>
2021-04-01 18:31:57 +0200

    Add support for Firework's custo components (including fw-storyblock)

--- src/index.js
+++ src/index.js
@@ -3,49 +3,40 @@ import PropTypes from 'prop-types'
 import Script from 'react-load-script'
 
 const ReactFWN = (props) => {
-  const { Fragment, useLayoutEffect, useRef, useState } = React // Only import defaults from modules, then decompose. This is due to CRA's webpack not working properly with esm modules.
-  const { script_url, api_host, ...rest } = props || {}
+  const { Fragment, useState } = React // Only import defaults from modules, then decompose. This is due to CRA's webpack not working properly with esm modules.
+  const { script_url, ...rest } = props || {}
   const [scriptLoaded, setScriptLoaded] = useState(false)
   const [scriptError, setScriptError] = useState(false)
-  const containerRef = useRef(null)
-
-  useLayoutEffect(() => {
-    if (scriptLoaded && containerRef.current) {
-      const options = {
-        ...rest,
-        target: containerRef.current,
-      }
-      if (api_host) {
-        options.api_host = api_host
-      }
-      window._fwn && window._fwn.render && window._fwn.render(options)
-    }
-  }, [scriptLoaded, containerRef.current, props])
 
   return (
     <Fragment>
       {!scriptLoaded && (
         <Script
-          url={script_url || 'https://asset.fwcdn1.com/js/fwn.js'}
-          attributes={{ id: 'fwn_script' }}
+          url={script_url || 'https://asset.fwcdn2.com/js/fwn.js'}
           onError={() => setScriptError(true)}
           onLoad={() => setScriptLoaded(true)}
         />
       )}
-      <div
-        className="fwn_videos"
-        style={{ width: '100%' }}
-        ref={containerRef}
-      />
       {scriptError && <div>Firework Network script failed to load</div>}
+      {rest.mode === 'story' ? (
+        <fw-storyblock {...rest}></fw-storyblock>
+      ) : (
+        <fw-embed-feed {...rest}></fw-embed-feed>
+      )}
     </Fragment>
   )
 }
 
 ReactFWN.propTypes = {
   app_id: PropTypes.string,
+  channel: PropTypes.string,
+  hashtag: PropTypes.string,
+  playlist: PropTypes.string,
+  video: PropTypes.string,
   mode: PropTypes.string,
   open_in: PropTypes.string,
+  height: PropTypes.string,
+  size: PropTypes.string,
   title: PropTypes.string,
   autoplay: PropTypes.bool,
   placement: PropTypes.string,
--- src/index.test.js
+++ src/index.test.js
@@ -3,9 +3,27 @@ import { mount } from 'enzyme'
 import Script from 'react-load-script'
 import ReactFWN from './index'
 
-test('ReactFWN renders just fine', () => {
-  const wrapper = mount(<ReactFWN />)
-  const scriptTag = wrapper.find(Script)
+describe('ReactFWN component', () => {
+  test("should inject component's script", () => {
+    const wrapper = mount(<ReactFWN />)
 
-  expect(scriptTag.props().url).toEqual('https://asset.fwcdn1.com/js/fwn.js')
+    expect(wrapper.find(Script).props().url).toEqual(
+      'https://asset.fwcdn2.com/js/fwn.js'
+    )
+  })
+
+  test('should render default <fw-embed-feed /> custom component', () => {
+    const wrapper = mount(<ReactFWN arg1="value1" arg2="value2" />)
+
+    expect(wrapper.find('fw-embed-feed')).toMatchSnapshot()
+    expect(wrapper.find(Script).props().url).toEqual(
+      'https://asset.fwcdn2.com/js/fwn.js'
+    )
+  })
+
+  test('should render <fw-storyblock /> custom component for mode=story', () => {
+    const wrapper = mount(<ReactFWN mode="story" arg1="value1" arg2="value2" />)
+
+    expect(wrapper.find('fw-storyblock')).toMatchSnapshot()
+  })
 })
