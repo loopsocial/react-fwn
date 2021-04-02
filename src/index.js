2beddb6db524a3de9a84ed3e0db90baf9dccdd72
Štefan Bačkor <stevo@backor.sk>
2021-04-02 22:12:43 +0200

    Fix import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 const ReactFWN = (props) => {
-  const { Fragment } = React // Only import defaults from modules, then decompose. This is due to CRA's webpack not working properly with esm modules.
+  const { Fragment, useEffect } = React // Only import defaults from modules, then decompose. This is due to CRA's webpack not working properly with esm modules.
   const { script_url, ...rest } = props || {}
 
   useEffect(() => {
